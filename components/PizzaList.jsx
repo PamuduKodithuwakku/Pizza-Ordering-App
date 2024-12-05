be34c97a02f7e7f9f885e08b8c97491e5ea9eeef
Pamudu Kodithuwakku <pamudupankaja2002@gmail.com>
2024-12-05 21:25:39 +0530

    eightth commit

--- components/PizzaList.jsx
+++ components/PizzaList.jsx
@@ -7,7 +7,7 @@ const PizzaList = ({ pizzaList }) => {
         <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
         <p className={styles.desc}>
             Each slice is a journey through layers of flavor, where handcrafted dough meets the freshest ingredients to create something truly exceptional. 
-            Whether it's the comforting simplicity of a Margherita or the bold zest of a gourmet creation, our pizzas are a celebration of tradition and innovation. 
+            Whether it&apos;s the comforting simplicity of a Margherita or the bold zest of a gourmet creation, our pizzas are a celebration of tradition and innovation. 
             Savor the perfect blend of crispy crust, rich sauce, and mouthwatering toppings, and let every bite transport you to the heart of Italy.
         </p>
         <div className={styles.wrapper}>
